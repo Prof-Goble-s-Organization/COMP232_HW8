f4151a435cfcd24ab39e21534f95b91d5336bb6d
wpgoble <will.goble@gmail.com>
2024-04-23 10:28:05 -0400

    adds base file

---
+++ src/Lambdas.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.stream.IntStream;
+
+public class Lambdas {
+    public static void printSquares(int start, int end) {
+        
+    }
+
+    public static void printEvenIntegers(int start, int end) {
+
+    }
+
+    public static void squareOfIntegers(int start, int end) {
+
+    }
+
+    public static void sumOfCubes(int start, int end) {
+
+    }
+
+    public static ArrayList<Integer> factors(int i) {
+        return null;
+    }
+
+    public static void factorsParallelStream(int i) {
+
+    }
+
+    public static void main(String[] args) {
+        printSquares(0,100);
+		printEvenIntegers(0,100);
+		squareOfIntegers(0,10);
+		sumOfCubes(0,10);
+		System.out.println(factors(12));
+		factorsParallelStream(12);
+    }
+}
